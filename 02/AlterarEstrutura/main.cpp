25ae18199d152da0e7e18388460bbf64af859914
danilosgomes <gomesdanilo8@gmail.com>
2023-05-19 22:48:28 -0300

    ED - 02

---
+++ 02/AlterarEstrutura/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+struct aluno{
+  float nota[3], media;
+};
+
+void calc_media(aluno *user){
+  user -> media = ((user -> nota[0] + user -> nota[1] + user -> nota[2])/3);
+}
+
+int main(){
+  cout.precision(1);
+  aluno user;
+  int i{0};
+    while(i < 3){
+      cin >> user.nota[i];
+      i++;
+    }
+  calc_media(&user);
+
+  cout << fixed << user.media;
+}
