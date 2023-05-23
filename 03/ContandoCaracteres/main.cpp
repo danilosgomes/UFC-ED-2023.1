939e48449c81435a8cde5825f19d781bd967ee36
danilosgomes <gomesdanilo8@gmail.com>
2023-05-23 10:28:49 -0300

    ED - 03

---
+++ 03/ContandoCaracteres/main.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string>
+
+
+using namespace std;
+
+// Retorna o números de ocorrências do caractere 'c' na string 'str' (com 'n' caracteres).
+// Algoritmo deve ser recursivo e sem comandos de repetição.
+int contaCaracteres(string str, int n, char c){
+    // fazer
+  int numC {};
+  for(int i {}; i < n; i++){
+    if(str[i] == c){
+      numC++;
+    }
+  }
+  return numC;
+}
+
+int main(){
+   // fazer
+  string str;
+  getline(cin,str);
+  char c {};
+  cin >> c;
+  cout << contaCaracteres(str, (int)str.size(), c);
+}
